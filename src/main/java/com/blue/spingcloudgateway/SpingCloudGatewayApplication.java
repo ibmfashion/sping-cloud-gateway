9779053f12904f2bb53d2f6355b597e3e05805e5
getech <aligoods@qq.com>
2020-05-22 16:04:20 +0800

    spring-cloud-gateway

---
+++ src/main/java/com/blue/spingcloudgateway/SpingCloudGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.blue.spingcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingCloudGatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpingCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blue/spingcloudgateway/SpingCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blue.spingcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
